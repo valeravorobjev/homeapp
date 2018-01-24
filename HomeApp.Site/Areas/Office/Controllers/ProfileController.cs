b6eeeaa68ac2458daed49223a8497a6989ca8583
boniface <info@rabun.ru>
2018-01-24 19:30:13 +0300

    Начало работы над формой SetPersonProfile

--- HomeApp.Site/Areas/Office/Controllers/ProfileController.cs
+++ HomeApp.Site/Areas/Office/Controllers/ProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using HomeApp.Core.Db.Entities;
+using HomeApp.Core.Db.Entities.Models;
 using HomeApp.Core.Db.Entities.Models.Enums;
 using HomeApp.Core.Repositories.Contracts;
 using HomeApp.Site.Areas.Office.ViewModels;
@@ -65,13 +66,32 @@ namespace HomeApp.Site.Areas.Office.Controllers
         [HttpGet]
         public async Task<IActionResult> SetPersonProfile(UserType userType)
         {
-            SetPersonProfileViewModel model = new SetPersonProfileViewModel {UserType = userType};
+            Person user = (Person)await _userRepository.GetUserAsync(HttpContext.User.Identity.Name);
+
+            SetPersonProfileViewModel model = new SetPersonProfileViewModel
+            {
+                UserId = user.Id.ToString(),
+                UserType = userType,
+                FirstName = user.FirstName?.Ru,
+                LastName = user.LastName?.Ru,
+                MidName = user.MidName?.Ru,
+                DateBirth = user.DateBirth,
+                Sex = user.Sex
+            };
             return View(model);
         }
 
         [HttpPost]
         public async Task<IActionResult> SetPersonProfile(SetPersonProfileViewModel model)
         {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            _userRepository.SetPersonAsync(new ObjectId(model.UserId), new Person
+            {
+
+            });
+
             if (model.UserType == UserType.Realtor)
                 return RedirectToAction("SetRealtorProfile", new { model.UserType });
 
--- HomeApp.Site/Areas/Office/ViewModels/SetPersonProfileViewModel.cs
+++ HomeApp.Site/Areas/Office/ViewModels/SetPersonProfileViewModel.cs
@@ -1,9 +1,20 @@
-﻿using HomeApp.Core.Db.Entities.Models.Enums;
+﻿using System;
+using System.Collections.Generic;
+using HomeApp.Core.Db.Entities.Models.Enums;
+using MongoDB.Bson;
 
 namespace HomeApp.Site.Areas.Office.ViewModels
 {
     public class SetPersonProfileViewModel
     {
+        public string UserId { get; set; }
         public UserType UserType { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MidName { get; set; }
+        public DateTime DateBirth { get; set; }
+        public bool? Sex { get; set; }
+        public List<string> Phones { get; set; }
+        public string Address { get; set; }
     }
 }
